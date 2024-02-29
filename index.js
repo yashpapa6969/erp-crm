c6a19c61d4be43d7a273ac615565a5d1d0af9fcc
yashpapa6969 <yashgupta.yg856@gmail.com>
2024-02-29 23:32:49 +0530

    yash

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.get('/', (req, res) => {
   // Send the response with a JSON object containing the "message"
-  res.json({ message: '!kmkmnjk' });
+  res.json({ message: '!welcome' });
 });
 
 app.use('/api/admin', adminRoutes);
