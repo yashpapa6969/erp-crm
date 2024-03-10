50e5f17cbd90829e807912413d9632735bd6a981
yashpapa6969 <yashgupta.yg856@gmail.com>
2024-03-10 21:21:45 +0530

    anniversary

--- controllers/Clients/clientDateReminder.js
+++ controllers/Clients/clientDateReminder.js
@@ -1,33 +1,47 @@
 const schemas = require("../../mongodb/schemas/schemas");
+const moment = require('moment'); 
+const convertToDateObject = (dateStr) => {
+    return moment(dateStr, 'DD-MM-YY').toDate();
+};
 
-const getOneMonthFromNowRange = () => {
-    const today = new Date();
-    const oneMonthFromNow = new Date(new Date().setMonth(today.getMonth() + 1));
-    today.setHours(0, 0, 0, 0);
-    oneMonthFromNow.setHours(23, 59, 59, 999);
-  
-    return { today, oneMonthFromNow };
-  };
-  
+const isWithinNext30Days = (date) => {
+    const today = moment().startOf('day'); 
+    const thirtyDaysLater = moment().add(30, 'days').endOf('day'); 
+    return date.isBetween(today, thirtyDaysLater, null, '[]'); 
+};
 
-  specialDates= async (req, res) => {
-    const { today, oneMonthFromNow } = getOneMonthFromNowRange();
-    
+
+const filterClientsBySpecialDates = (clients) => {
+    return clients.map(client => {
+        let upcomingEvents = [];
+
+        if (isWithinNext30Days(moment(client.clientBirthday, 'DD-MM-YY'))) {
+            upcomingEvents.push('clientBirthday');
+        }
+        if (isWithinNext30Days(moment(client.clientAnniversary, 'DD-MM-YY'))) {
+            upcomingEvents.push('clientAnniversary');
+        }
+        if (isWithinNext30Days(moment(client.workStartDate, 'DD-MM-YY'))) {
+            upcomingEvents.push('workStartDate');
+        }
+        if (isWithinNext30Days(moment(client.companyAnniversary, 'DD-MM-YY'))) {
+            upcomingEvents.push('companyAnniversary');
+        }
+
+        if (upcomingEvents.length > 0) {
+            return { ...client._doc, upcomingEvents }; 
+        } else {
+            return null;
+        }
+    }).filter(client => client !== null);
+};
+const specialDates = async (req, res) => {
     try {
-      const clients = await schemas.Client.find({
-        $or: [
-          { clientBirthday: { $gte: today, $lte: oneMonthFromNow } },
-          { clientAnniversary: { $gte: today, $lte: oneMonthFromNow } },
-          { workStartDate: { $gte: today, $lte: oneMonthFromNow } },
-          { companyAnniversary: { $gte: today, $lte: oneMonthFromNow } }
-        ]
-      });
-  
-      res.json(clients);
+        const clients = await schemas.Client.find(); 
+        const clientsWithUpcomingSpecialDates = filterClientsBySpecialDates(clients);
+        res.json(clientsWithUpcomingSpecialDates);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
-  };
-  
-  module.exports = specialDates;
-    
+};
+module.exports  = specialDates
