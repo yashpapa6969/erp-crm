7cfee76b9c1f91a47f866ff29a7547f23786180c
yashpapa6969 <yashgupta.yg856@gmail.com>
2024-04-08 15:14:51 +0530

    yash

--- controllers/Duration/deleteDuration.js
+++ controllers/Duration/deleteDuration.js
@@ -5,7 +5,7 @@ const deleteDurationById = async (req, res) => {
   const duration_id = req.params.duration_id;
 
   try {
-    const duration = await schemas.Year.findOne({duration_id:duration_id});
+    const duration = await schemas.Duration.findOne({duration_id:duration_id});
 
     if (!duration) {
       return res.status(404).json({ message: "duration not found." });
